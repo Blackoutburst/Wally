d0d2b2192a24e30f0a37fc6a15d5e7b87d08ef45
Blackoutburst <30992311+Blackoutburst@users.noreply.github.com>
2020-10-23 15:38:17 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import javax.security.auth.login.LoginException;
 import core.Bot;
 
 public class Main {
-	private static final String TOKEN = "X";
+	private static final String TOKEN = "NzYzMTQzMTExNzE4ODYyODQ5.X3zaaA.K7ZkFAW5wRLZJbaPBiHhjbm3TbE";
 	private static final String ACTIVITY = "!help";
 	public static final String bypassID = "X";
 	public static final String serverID = "X";
