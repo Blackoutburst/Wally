063bbca72c77dcbac176ee1ac685a5b65e5752a6
Blackoutburst <blackoutburst@hotmail.com>
2021-03-14 15:08:56 +0100

    Fixed common error on join event when wally is running on multiple server at once

--- src/event/GuildMemberJoin.java
+++ src/event/GuildMemberJoin.java
@@ -1,5 +1,6 @@
 package event;
 
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import utils.Config;
 
@@ -12,6 +13,10 @@ public class GuildMemberJoin {
 	public void run(GuildMemberJoinEvent event) {
 		if (event.getMember().getUser().isBot()) return;
 		
-		event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById(Config.getRoleId("Members"))).complete();
+		Role role = event.getGuild().getRoleById(Config.getRoleId("Members"));
+		
+		if (role == null) return;
+		
+		event.getGuild().addRoleToMember(event.getMember(), role).complete();
 	}
 }
